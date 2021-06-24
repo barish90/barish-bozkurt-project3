b09565414cd3639345d23214f5f6da231d78e4ba
barish90 <baris.kutsal@gmail.com>
2021-06-23 22:05:50 -0400

    added video backgtound and finall styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
 import PlantTree from './PlantTree';
 import Planters from './Planters';
+import bg from './images/bg.mp4'
 
 function App() {
-
-  
-
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tree Planter</h1>
-        <h3>Plant a tree to curb the effects of climate change</h3>
+        <video autoPlay loop muted>
+          <source src={bg} type="video/mp4"/>
+        </video>
       </header>
-      <PlantTree>
-      </PlantTree>
-      <Planters/>
+      <div className="content">
+        <h1>Tree Planter</h1>
+        <p>Plant a tree to curb the effects of climate change</p>
+      </div>
+      <PlantTree />
+      <Planters />
       <footer>
         <p>Created by Barish Bozkurt at Juno College</p>
       </footer>
--- src/PlantTree.js
+++ src/PlantTree.js
@@ -1,57 +1,47 @@
 import firebase from './firebase';
-import {useState} from 'react';
+import { useState } from 'react';
 import './Plant.css';
-import { useLottie } from "lottie-react";
-import animation from "./newdata.json";
+import Lottie from "lottie-react"; // importing Lottie library
+import animation from "./newdata.json"; //importing animation file
 
 function PlantTree () {
     const dbRef = firebase.database().ref(); // declaring db ref
 
-    const [userInput, setUserInput] = useState();
+    // use state to take userinput
+    const [userInput, setUserInput] = useState(''); 
+    // useState to set animation to run
+    const [showAnimation,  setShowAnimation] = useState(false) 
 
+    // function to handle the change on input field
     const handleChange = (event) => {
         setUserInput(event.target.value)
       }
 
+    // function to handle the click event  
     const handleClick = (e) => {
         e.preventDefault();
         dbRef.push(userInput);
-
-        // 
+        setShowAnimation(true);
+        setUserInput('');
       }
-
-      const options = {
-        loop: false,
-        autoplay: true,
-        animationData: animation,
-      };
-      const style = {
-        height: 300,
-      };
-      const { View } = useLottie(options, style)
-
-      // const handleAnimation = () => {
-      //   animation.playSegments([0,60], true);
-      // }
-
-      // useEffect(() => {
-      //   lottie.loadAnimation({
-      //     container: document.querySelector("#tree-animation"),
-      //     animationData: newdata
-      //   });
-      // }, []);
-    
 
     return(
             <section className="App-plant-tree">
-              <form action="submit">
+              <div className="about">
+                <h2>Why we need to save tress?</h2>
+                <p>Trees help clean the air we breathe, filter the water we drink, and provide habitat to over 80% of the world's terrestrial biodiversity. They also provide jobs to over 1.6 billion people, absorb harmful carbon from the atmosphere, and are key ingredients in 25% of all medicines.</p>
+                <p>We are dedicated to making it easier for individuals and businesses to give back to the environment, create a healthier climate, protect biodiversity and help reforestation efforts around the world. All by planting trees!</p>
+                <h3>Plant a tree by filing out the from below</h3>
+              </div>
+              <form onSubmit={handleClick}>
                 <label htmlFor="planterName">Please enter your name</label>
-                <input onChange={handleChange} placeholder="Your name" type="text" id="planterName" value={userInput} required="required"/>
-                <button className="button"onClick={handleClick}>Plant a Tree</button>
-                {/* <div id="tree-animation" style={{ width: 200, height: 200 }}>
-                </div> */}
-                {/* <useLottie options={animation} style={{ width: 200, height: 400 }}/> */}
-                { View }
+                <input onChange={handleChange} placeholder="Your name" type="text" id="planterName" value={userInput} required/>
+                <button type="submit" className="button">Plant a Tree</button>
+                
+                {/* code to show the animation when button is clicked  */}
+                {
+                showAnimation ? <div className="animation"><Lottie animationData={animation} loop={false} style={{ width: 200, height: 200, marginTop: 40, paddingLeft: 150 }}/><p className="fadeInUp">Thank you for helping us to save our planet</p> </div> : <div className="animation"></div>
+                }
               </form>
             </section>
     )
